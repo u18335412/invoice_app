c837ef566cee2137680524163c8b9cf534474a04
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-03 20:14:33 +0200

    fixed Positionig for Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import Navbar from "../src/components/navbar";
 import Footer from "../src/components/footer";
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="relative">
       <Navbar></Navbar>
       <Component {...pageProps} />
       {/* <Footer></Footer> */}
-    </>
+    </div>
   );
 }
 
