091e417bcf45ac9933a3c32d94175cf33cf8e3f4
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-03 20:12:03 +0200

    container padding refactor

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,9 @@
 const Contanier = ({ children }) => {
-  return <div className="xl:px-[22.188rem] md:px-[3rem]">{children}</div>;
+  return (
+    <div className="xl:px-[22.188rem] md:px-[3rem]">
+      {children}
+    </div>
+  );
 };
 
 export default Contanier;
