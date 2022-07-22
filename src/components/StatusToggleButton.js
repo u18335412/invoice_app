e5ddadf124922430ba34f0a810df4c426d4654d7
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-22 22:31:32 +0200

    status toggle button

---
+++ src/components/StatusToggleButton.js
@@ -0,0 +1,12 @@
+const StatusToggleButton = ({ markAsStatus, invoiceId, invoiceStatus }) => {
+  return (
+    <button
+      onClick={() => markAsStatus(invoiceId)}
+      className=" font-bold px-4 md:px-[1.5rem] py-[1rem] bg-[rgba(124,93,250,1)] rounded-full h-[3rem] transition-all hover:bg-[rgba(146,119,255,1)] text-white text-[0.7rem]"
+    >
+      {invoiceStatus === "paid" ? "Mark as Pending" : "Mark as Paid"}
+    </button>
+  );
+};
+
+export default StatusToggleButton;
