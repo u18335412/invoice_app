f6f112b366cff9487425c8b3f67fc20283c6e5ac
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-22 22:29:18 +0200

    store for persisting theme state

---
+++ src/store/theme.js
@@ -0,0 +1,17 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+const useStore = create(
+  persist((set, get) => ({
+    isDark: true,
+    toggleTheme: () =>
+      set({
+        isDark: !get().isDark,
+      }),
+  })),
+  {
+    name: "theme-storage",
+  }
+);
+
+export default useStore;
