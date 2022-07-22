5007a1d7829dd2d860a1781246eccd44e7c12ce9
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-22 22:37:39 +0200

    disable button on draft status

--- src/components/StatusToggleButton.js
+++ src/components/StatusToggleButton.js
@@ -1,8 +1,9 @@
 const StatusToggleButton = ({ markAsStatus, invoiceId, invoiceStatus }) => {
   return (
     <button
+      disabled={invoiceStatus === "draft"}
       onClick={() => markAsStatus(invoiceId)}
-      className=" font-bold px-4 md:px-[1.5rem] py-[1rem] bg-[rgba(124,93,250,1)] rounded-full h-[3rem] transition-all hover:bg-[rgba(146,119,255,1)] text-white text-[0.7rem]"
+      className=" font-bold px-4 md:px-[1.5rem] py-[1rem] bg-[rgba(124,93,250,1)] rounded-full disabled:bg-gray-600 disabled:opacity-60 disabled:cursor-not-allowed h-[3rem] transition-all hover:bg-[rgba(146,119,255,1)] text-white text-[0.7rem]"
     >
       {invoiceStatus === "paid" ? "Mark as Pending" : "Mark as Paid"}
     </button>
