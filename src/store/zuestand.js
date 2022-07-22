9b08d8172b24fd51afc3b003fef44605bcf713e4
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-22 22:29:38 +0200

    global store for invoices

--- src/store/zuestand.js
+++ src/store/zuestand.js
@@ -1,21 +1,17 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 import data from "../../data.json";
 
 const useStore = create((set) => ({
   invoices: data,
-  isDark: false,
-  toggleTheme: () =>
-    set((state) => ({
-      isDark: !state.isDark,
-    })),
   addInvoice: (invoice) =>
     set((state) => ({
       invoices: [{ ...invoice }, ...state.invoices],
     })),
   editInvoice: (newInvoices) =>
-    set((state) => ({
+    set({
       invoices: newInvoices,
-    })),
+    }),
   removeInvoice: (id) =>
     set((state) => ({
       invoices: state.invoices.filter((invoice) => invoice.id !== id),
