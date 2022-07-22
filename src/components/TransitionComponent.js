425db8261ea8cc1f40f026146ce337266447184d
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-22 22:31:50 +0200

    Transition component

---
+++ src/components/TransitionComponent.js
@@ -0,0 +1,23 @@
+import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+const TransitionComponent = ({ showing, children, as }) => {
+  return (
+    <Transition
+      as={as || Fragment}
+      appear={true}
+      show={showing}
+      enter="transform transition duration-[200ms]"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transform duration-200 transition ease-in-out"
+      leaveFrom="opacity-100 scale-100 "
+      leaveTo="opacity-0 scale-90"
+      afterLeave={() => {}}
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export default TransitionComponent;
